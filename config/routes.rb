6806ef044d5347f6ca5bbd6798f3b5231a93b3d3
Joshua <jhurd59@academic.rrc.ca>
2020-09-30 18:59:53 -0500

    Added customer partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'customers#index'
+
+  resources :customers, only: [:index]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
