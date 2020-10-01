4e622d103b10379690bdd938fc285f7f04ed952e
Joshua <jhurd59@academic.rrc.ca>
2020-09-30 19:22:03 -0500

    Added alphabetized and missing email views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,12 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+
+  def alphabetized
+    @customers = Customer.order(:name)
+  end
+
+  def missing_email
+    @customers = Customer.where(email: '')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   root to: 'customers#index'
 
   resources :customers, only: [:index]
+  resources :customers do
+    collection do
+      get 'alphabetized'
+      get 'missing_email'
+    end
+  end
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
